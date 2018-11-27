af675de3b8464cf1fd447c96c5adc256710c8e13
ntibbals <ntibbals@outlook.com>
2018-11-26 18:24:55 -0800

    completed arrow functions

--- 00-prep/lab/1-ES6-practice/arrow-functions/app.js
+++ 00-prep/lab/1-ES6-practice/arrow-functions/app.js
@@ -178,17 +178,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-// 
+// this pulls in and defines the object which is Student
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+//  This is pulling from the parent container which in this example is Window.
 // 3. Explain why "this" is different when an arrow function is used.
-// 
+// arrow functions do not bind its own this arguments as arrow functions are always anonymous.
